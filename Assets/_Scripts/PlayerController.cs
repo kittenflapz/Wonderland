b88a22091365f029590d7faebda79d8bf9311abd
Catt Symonds <catherine.symonds.thompson@gmail.com>
2020-12-08 13:05:47 -0500

    flesh out player movement

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -12,32 +12,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rigidbody;
+    Animator animator;
+
+    [SerializeField]
+    Image forceMeter;
 
     [SerializeField]
     float force;
+
+    [SerializeField]
+    float maxForce;
+
+    bool flying;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = gameObject.GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        forceMeter.fillAmount = force / maxForce;
+
+        animator.SetFloat("MoveVectorMagnitude", rigidbody.velocity.magnitude);
+
+        flying = rigidbody.velocity.magnitude > float.Epsilon ? true : false;
+
+
         if (!SceneButtonManager.gameIsPaused)
         { // If screen is touched
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButton(0))
             {
-                Debug.Log("adding force");
-                // Add a velocity in that direction
-                Vector2 playerToTarget = Camera.main.ScreenToWorldPoint(Input.mousePosition) - gameObject.transform.position;
-                rigidbody.AddForce(playerToTarget * force);
+                if (!flying)
+                {
+                    if (force < maxForce)
+                    { force++; }
+                }
 
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                if (!flying)
+                {
+                    // Add a velocity in that direction
+                    Vector2 playerToTarget = Camera.main.ScreenToWorldPoint(Input.mousePosition) - gameObject.transform.position;
+                    rigidbody.AddForce(playerToTarget * force);
+                    force = 0;
+                }
             }
             if (transform.position.y < -10.0f)
             {
